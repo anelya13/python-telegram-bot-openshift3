06a2fcefd6f55bd17518ea0e58bb7ba5cf5a1ee3
anelya13 <44569406+anelya13@users.noreply.github.com>
2018-11-20 12:27:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -33,56 +33,16 @@ def help(bot, update):
 def button(bot, update):
     query = update.callback_query
     if query.data == "1":
-        text = "You can choose any of the following actions: +, -, /, *"
+        text = "Song 1"
     elif query.data == "2":
         text = "3+4, 44-12, 43/2, 12*90"
     bot.editMessageText(text=text, chat_id=query.message.chat_id,
                         message_id=query.message.message_id)
 
 
-class arr:
-
-    def __init__(self, num, random_num=random.randint(1, 100)):
-        self.random_num = random_num
-        self.num = num
-
-    def checking(self, bot, update):
-        try:
-            if self.random_num == self.num:
-                ar = 'Вы выиграли!'
-            elif self.random_num > self.num:
-                ar = 'Ваше число меньше моего. Попробуйте еще раз :)'
-            elif self.random_num < self.num:
-                ar = 'Ваше число больше моего. Попробуйте еще раз :)'
-            # ar = int(num) * 370
-        except (NameError, SyntaxError, ValueError):
-            ar = "Введите целое число"
-        bot.send_message(chat_id=update.message.chat_id, text=ar)
-
-
-def guessing(bot, update):
-    input_num = int(update.message.text)
-    object_num = arr()
-    object_num.checking(input_num)
-
-
-"""def echo(bot, update):
-    update.message.reply_text('You said:  ' + update.message.text)
-"""
-
-"""def dolintenge(bot, update, args):
-    dollars = int(args)
-    tenge = dollars * 356
-    update.message.reply_text(text=tenge)
-"""
-
-
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"' % (update, error))
-
-
-# Write your handlers here
 
 
 def setup(webhook_url=None):
@@ -96,14 +56,9 @@ def setup(webhook_url=None):
         updater = Updater(TOKEN)  # Create the EventHandler and pass it your bot's token.
         bot = updater.bot
         dp = updater.dispatcher  # Get the dispatcher to register handlers
-        dp.add_handler(CommandHandler("start", start))  # on /start command answer in Telegram
+        dp.add_handler(CommandHandler("open", open))  # on /start command answer in Telegram
         dp.add_handler(CommandHandler("help", help))  # on /help command answer in Telegram
-        """dp.add_handler(CommandHandler("dolintenge", dolintenge))"""
         dp.add_handler(CallbackQueryHandler(button))
-
-        # on noncommand i.e message - echo the message on Telegram
-        """dp.add_handler(MessageHandler(Filters.text, echo))"""
-        dp.add_handler(MessageHandler(Filters.text, guessing))
 
         # log all errors
         dp.add_error_handler(error)
